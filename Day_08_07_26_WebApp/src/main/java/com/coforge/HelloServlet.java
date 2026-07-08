517854963d3ad89de7b4408508dde6e5aa18cc7e
brianpeterjames <brian.peter@coforge.com>
2026-07-09 01:24:44 +0530

    Web app adding

---
+++ Day_08_07_26_WebApp/src/main/java/com/coforge/HelloServlet.java
@@ -0,0 +1,45 @@
+package com.coforge;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		out.println("Hello Peter");
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Day_08_07_26_WebApp/target/Day_08_07_26_WebApp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2><%= "Hello World!" %></h2>
+<form action="HelloServlet">
+<button>Click</button>
+</form>
+
+
+</body>
+</html>
