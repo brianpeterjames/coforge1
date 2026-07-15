e0a7ae5546d558712389442eb1dd6464c8d93f61
brianpeterjames <brian.peter@coforge.com>
2026-07-15 09:09:47 +0530

    13July

---
+++ Day_13_07_26_filter/src/main/java/com/coforge/HelloServlet.java
@@ -0,0 +1,54 @@
+package com.coforge;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+//		out.println("<h1>Start Time"+LocalDate.now()+"</h1");
+//		out.println("URL "+request.getRequestURL());
+//		out.println("Method Name :"+request.getMethod());
+//		out.println("Remote Addr:"+request.getRemoteAddr());
+//		out.println("<h1>End Time :"+LocalDateTime.now()+"</h1>");
+		
+		out.print("<h1>This is Hello Servlet</h1>");
+		out.close();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Day_13_07_26_filter/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+<link
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+rel="stylesheet">
+</head>
+<body>
+<h2><%= "Demo On Filter" %></h2>
+<a class="btn btn-primary" href = "HelloServlet">Hello</a>
+<a class="btn btn-success" href = "WelcomeServlet">Welcome</a>
+</body>
+</html>
