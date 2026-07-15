0cd6b6a76399631708c34612015c17d9b0622889
brianpeterjames <brian.peter@coforge.com>
2026-07-15 09:03:27 +0530

    9 July Work

---
+++ Day_09_07_26_WebApp/src/main/java/com/coforge/LoginServlet.java
@@ -0,0 +1,61 @@
+package com.coforge;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Login Application");
+		String userName = request.getParameter("uname");
+		String password = request.getParameter("pwd");
+		System.out.println(userName);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("welcome.jsp");
+		
+		// authentication
+		if(userName.equals("Brian") && password.equals("Bjpop@22"))
+		{
+			request.setAttribute("userName", userName);
+			rd.forward(request, response);
+			
+		}
+		
+		else
+			rd =request.getRequestDispatcher("error.jsp");
+			rd.forward(request, response);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
